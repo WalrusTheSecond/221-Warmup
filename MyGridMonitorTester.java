b513a6bc28304c18f218a0c4351206d9b373971d
Evan Wallace <evanwallace@u.boisestate.edu>
2025-01-24 16:39:10 -0700

    Submitted

--- MyGridMonitorTester.java
+++ MyGridMonitorTester.java
@@ -2,7 +2,7 @@ import java.io.FileNotFoundException;
 
 public class MyGridMonitorTester{
     public static void main(String[] args) {
-        String test = "wideRange.txt";
+        String test = "$R0IV5S9.txt";
         try{
         GridMonitor gm = new GridMonitor(test);
         System.out.println(gm.toString());
